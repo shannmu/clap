14ba9935ac1d49888b41012d8b1c9032300cb752
shannmu <shanmu1901@gmail.com>
2024-10-13 21:55:31 +0800

    nightly update

--- clap_complete/src/env/shells.rs
+++ clap_complete/src/env/shells.rs
@@ -29,8 +29,11 @@ impl EnvCompleter for Bash {
 
         let script = r#"
 _clap_complete_NAME() {
+    local cur prev words cword
+    _init_completion -n =
+
     local IFS=$'\013'
-    local _CLAP_COMPLETE_INDEX=${COMP_CWORD}
+    local _CLAP_COMPLETE_INDEX=${cword}
     local _CLAP_COMPLETE_COMP_TYPE=${COMP_TYPE}
     if compopt +o nospace 2> /dev/null; then
         local _CLAP_COMPLETE_SPACE=false
@@ -42,13 +45,8 @@ _clap_complete_NAME() {
         _CLAP_COMPLETE_INDEX="$_CLAP_COMPLETE_INDEX" \
         _CLAP_COMPLETE_COMP_TYPE="$_CLAP_COMPLETE_COMP_TYPE" \
         VAR="bash" \
-        "COMPLETER" -- "${COMP_WORDS[@]}" \
+        "COMPLETER" -- "${words[@]}" \
     ) )
-    if [[ $? != 0 ]]; then
-        unset COMPREPLY
-    elif [[ $SUPPRESS_SPACE == 1 ]] && [[ "${COMPREPLY-}" =~ [=/:]$ ]]; then
-        compopt -o nospace
-    fi
 }
 if [[ "${BASH_VERSINFO[0]}" -eq 4 && "${BASH_VERSINFO[1]}" -ge 4 || "${BASH_VERSINFO[0]}" -gt 4 ]]; then
     complete -o nospace -o bashdefault -o nosort -F _clap_complete_NAME BIN
